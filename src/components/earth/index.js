f103080cecd925b4061bcff0f3733028593ea3c9
rgallinocds <95765444+rgallinocds@users.noreply.github.com>
2021-12-28 15:01:29 -0300

    Updating files and readme

--- src/components/earth/index.js
+++ src/components/earth/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { useLoader } from "@react-three/fiber";
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, Stars} from '@react-three/drei'
 
 import EarthDayMap from "../../assets/textures/8k_earth_daymap.jpg";
 import EarthNormalMap from "../../assets/textures/8k_earth_normal_map.jpg";
 import EarthSpecularMap from "../../assets/textures/8k_earth_specular_map.jpg";
 import EarthCloudsMap from "../../assets/textures/8k_earth_clouds.jpg";
 import { TextureLoader } from "three";
+import * as TRHEE from 'three';
+
 
 export function Earth(props) {
 
@@ -14,11 +16,33 @@ export function Earth(props) {
 
     return (
     <>
-        <ambientLight intensity={0.5} />
+        {/* <ambientLight intensity={0.5} /> */}
+        <pointLight color="#f6f3ea" position={[2, 0, 2]} intensity={1.2} />
+
+        <Stars 
+            radius={300} 
+            depth={60} 
+            count={20000} 
+            factor={7} 
+            saturation={2} 
+            fade={true} />
+        <mesh>
+            <sphereGeometry args={ [ 1.005, 32, 32 ] } />
+            <meshPhongMaterial 
+                map={cloudsMap} 
+                opacity={0.4} 
+                depthWrite={true} 
+                transparent={true}
+                side={TRHEE.DoubleSide}/>
+        </mesh>
         <mesh>
             <sphereGeometry args={ [ 1, 32, 32 ] } />
             <meshPhongMaterial specularMap={specularMap} />
-            <meshStandardMaterial map={colorMap} normalMap={normalMap} />
+            <meshStandardMaterial 
+                map={colorMap} 
+                normalMap={normalMap} 
+                metalness={0.4} 
+                roughness={0.7}/>
             <OrbitControls
                 enableZoom={true}
                 enablePan={true}
